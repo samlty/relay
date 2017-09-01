a864ad1b0c6f5b77c30b643f0c89fa714ac6c9b4
xujianhua <xujianhua@bj.xinwei.com.cn>
2017-09-01 15:37:29 +0800

    add print

--- udproxy.py
+++ udproxy.py
@@ -1,13 +1,10 @@
-mport os
+import os
 import socket
 import threading
 import time
 
 class udp_work_server:
     def __init__(self, port):
-
-
-
         self.__listenPort = port
         self.__init_socket(port)
         self.__sourceAddrSocketMap = {}
@@ -19,6 +16,7 @@ class udp_work_server:
     def __init_socket(self, port):
         self.__sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.__sock.bind(('0.0.0.0', port))
+        print ("bind 0.0.0.0:" + port + "successful")
 
     def __get_list_item(self, address):
         for addr, value in self.__sourceAddrSocketMap.items():
@@ -33,20 +31,14 @@ class udp_work_server:
             data, clientAddr = self.__sock.recvfrom(1024 * 10)
             item = self.__get_list_item(clientAddr)
             if item is None:
+                print ("clientaddr " + str(clientAddr) + " not existed new one instance")
                 transInstance = udp_threading(self.__sock, clientAddr, self.__destAddr)
                 transInstance.start()
                 self.__sourceAddrSocketMap[clientAddr] =transInstance
             else:
                 transInstance = item
+
             transInstance.sendDataToDest(data)
-
-
-
-
-
-
-
-
 
 
 class udp_threading (threading.Thread):
@@ -55,21 +47,19 @@ class udp_threading (threading.Thread):
         self.__mainSock = mainSock
         self.__sourceAddr = sourceAddr
         self.__destAddr = destAddr
-        print "new a socker to recv/send data with soureAddr "
-        print self.__sourceAddr
-        print "start new thread"
+        print ("new a socker to recv/send data with soureAddr ")
+        print (self.__sourceAddr)
+        print ("start new thread")
 
         self.__localsock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        print "create localsock successful"
+        print ("create localsock successful")
 
     def run(self):
 
-
-
         while True:
             data, address = self.__localsock.recvfrom(1024 * 10)
-            print "recv data from dest"
-            print address
+            print ("recv data from dest")
+            print (address)
             self.__sendDataBackSourceUseMainSock(data)
 
 
