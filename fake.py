efa3c921de50dc8c186218f5f50b5bd46ef98917
xujianhua <xujianhua@bj.xinwei.com.cn>
2017-09-04 16:17:05 +0800

    adfaasdfasdf

--- fake.py
+++ fake.py
@@ -2,22 +2,38 @@ from scapy.all import *
 import sys
 import threading
 import time
+import fcntl
+import struct
+import socket
 
-def sendToPorts():
+def getIpFromInterface(ifname):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    return socket.inet_ntoa(fcntl.ioctl(
+        s.fileno(),
+        0x8915,
+        struct.pack('512s', ifname[:15])
+    )[20:24])
+
+
+def sendToPorts(ifname, hostname):
     data = "hello"
+
+    localIp = getIpFromInterface(ifname)
+    remoteIp = socket.gethostbyname(hostname)
+
     while True:
         for i in range(45600,45700):
-            pkt = IP(src=sys.argv[1], dst=sys.argv[2])/UDP(sport=1194, dport=i)/data
+            pkt = IP(src=localIp, dst=remoteIp)/UDP(sport=1194, dport=i)/data
             send(pkt)
     
     time.sleep(10)
 
 if __name__ == "__main__":
     if len(sys.argv)!=3:
-        print("use sourip destip ")
+        print("use interfaceName destip/hostname ")
         sys.exit(1)
 
-    t = threading.Thread(target=sendToPorts)
+    t = threading.Thread(target=sendToPorts, args=(sys.argv[1],sys.argv[2],))
     t.start()
 
     while True:
