fc30e9f75e585b3b0548d01558dff3c6d56d88c3
samlty <samlty.xu@gmail.com>
2017-09-04 13:04:39 +0800

    add fake

--- fake.py
+++ fake.py
@@ -1,13 +1,16 @@
 from scapy.all import *
 import sys
 import threading
+import time
 
 def sendToPorts():
     data = "hello"
     while True:
         for i in range(45600,45700):
             pkt = IP(src=sys.argv[1], dst=sys.argv[2])/UDP(sport=1194, dport=i)/data
-            send(pkt,inter=10,count=10000)
+            send(pkt)
+    
+    time.sleep(10)
 
 if __name__ == "__main__":
     if len(sys.argv)!=3:
@@ -19,7 +22,8 @@ if __name__ == "__main__":
 
     data = "hello"
     while True:
+	time.sleep(10)
         
-        pkt = IP(src=sys.argv[1], dst=sys.argv[2])/UDP(sport=1194, dport=8000)/data
-        send(pkt,inter=10,count=10000)
+        #pkt = IP(src=sys.argv[1], dst=sys.argv[2])/UDP(sport=1194, dport=8000)/data
+        #send(pkt,inter=10,count=10000)
 
