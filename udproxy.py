0c5c624bb05d5f32a2090338d8889cb980e759c5
xujianhua <xujianhua@bj.xinwei.com.cn>
2017-09-04 14:02:51 +0800

    adfaasdfasdf

--- udproxy.py
+++ udproxy.py
@@ -3,7 +3,13 @@ import socket
 import threading
 import time
 import sys
+import logging
 
+logging.basicConfig(level=logging.DEBUG,
+                format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
+                datefmt='%m-%d %H:%M:%S',
+                filename='myapp.log',
+                filemode='w')
 
 
 g_sockAddrList = [] # {} sock:sock, clientAddr:client addr count:int port:port of socket, serverAddr: vpn server addr
@@ -17,10 +23,10 @@ def init_socket(port):
     try:
         sock.bind(('0.0.0.0', port))
     except Exception:
-        print ("bind 0.0.0.0:" + str(port) + "failed")
+        logging.error ("bind 0.0.0.0:" + str(port) + "failed")
         return None
 
-    print ("bind 0.0.0.0:" + str(port) + "successful")
+    logging.error ("bind 0.0.0.0:" + str(port) + "successful")
     return sock
 
 
@@ -45,21 +51,23 @@ def getItemFromClientAddr(clientAddr):
     for item in g_sockAddrList:
         if item.has_key("clientAddr") and not cmp(item["clientAddr"], clientAddr):
             # print "successful"
+            logging.debug("found existed item use " + str(clientAddr))
             return item
-
+    logging.info(" not found existed item use " + str(clientAddr))
     return None
 
 def handleMsgFromClient(data, clientAddr):
-
+    logging.debug("recv data from client " + str(clientAddr))
     global g_sockAddrList
 
     infoItem = getItemFromClientAddr(clientAddr)
 
     if infoItem is None:
-        print (" not found with clientAddr" + str(clientAddr))
+        logging.info (" not found with clientAddr" + str(clientAddr))
 
         for item in g_sockAddrList:
             if not item.has_key("clientAddr"):
+                logging.info("get an unused item with port " + str(item["port"]))
 
                 item["clientAddr"] = clientAddr
                 item["count"] = 0
@@ -68,7 +76,7 @@ def handleMsgFromClient(data, clientAddr):
                 break
 
         if infoItem is None:
-            print "sock is full pls reboot app"
+            logging.error("sock is full pls reboot app")
             return
 
 
@@ -77,7 +85,7 @@ def handleMsgFromClient(data, clientAddr):
         localport = infoItem["port"]
         sock = infoItem["sock"]
 
-        print "recv data from " + str(clientAddr) + " send to " + str(serverAddr) + " use localport " + str(localport)
+        logging.debug( "recv data from " + str(clientAddr) + " send to " + str(serverAddr) + " use localport " + str(localport))
         sock.sendto(data, serverAddr)
 
 def serverSocketEntry(infoItem):
@@ -93,21 +101,27 @@ def serverSocketEntry(infoItem):
 
     while True: # if clientSocket Handle clean it, thread is over
         data, address = serverSock.recvfrom(1024 * 10)
+        logging.debug(" recv data from target " + str(address))
 
         if len(data) == 5 and str(data) == "hello":  # real
+            logging.debug("recv an hello msg")
             if not infoItem.has_key("serverAddr"):
+
                 infoItem["serverAddr"] = address
-                print "port " + str(localPort) + " add address " + str(address) + " to infoItem[\"serverAddr\"]"
+                logging.info( "port " + str(localPort) + " add address " + str(address) + " to infoItem[\"serverAddr\"]")
             #print "port " + str(localPort) + " recv hello from " + str(address)
+            elif cmp(infoItem["serverAddr"], address):
+                logging.info( "port " + str(localPort) + "renew address from " + str(infoItem["serverAddr"]) + " to " + str(address))
+                infoItem["serverAddr"] = address
 
 
         elif (len(data) == 4 and str(data) == "test"):
-            print  "port " + str(localPort) + "recv test from " + str(address)
+            logging.info(  "port " + str(localPort) + "recv test from " + str(address))
             serverSock.sendto("test", address)
 
         else:
             if infoItem.has_key("clientAddr"):
-                print "port " + str(localPort) + "recv data from " + str(address) + " send to " + str(infoItem["clientAddr"])
+                logging.debug( "port " + str(localPort) + "recv data from " + str(address) + " send to " + str(infoItem["clientAddr"]))
                 g_clientSock.sendto(data, infoItem["clientAddr"])
 
 
@@ -118,7 +132,7 @@ def checkCount():
         for item in g_sockAddrList:
             if item.has_key("count") and item.has_key("clientAddr"):
                 if item["count"] > 10:  # too old  , no traffic during 10*10s, clean it
-                    print "addr " + str(item["clientAddr"]) + " and localport " + str(item["port"]) + " is too old, clean it"
+                    logging.info( "addr " + str(item["clientAddr"]) + " and localport " + str(item["port"]) + " is too old, clean it")
                     del item["clientAddr"]
                     del item["count"]
                 else:
@@ -133,7 +147,7 @@ def main():
     g_clientSock = init_socket(1194)
 
     if g_clientSock is None:
-        print "bind 1194  failed"
+        logging.error( "bind 1194  failed")
         sys.exit(1)
 
 
@@ -142,16 +156,19 @@ def main():
     for i in range(45600,45700):
         sock = init_socket(i)
         if sock is None:
-            print "bind port %d failed continue" %i
+            logging.error( "bind port %d failed continue" %i)
             continue
+
         item={}
         item["sock"]=sock
         item["port"]=i
         g_sockAddrList.append(item)
         t = threading.Thread(target=serverSocketEntry, args=(item,))
         t.start()
+        logging.info("bind port " + str(i) + " && start thread succ")
 
     # start a thread to check too old client
+    logging.info("start checkout thread successful")
     timeThead = threading.Thread(target=checkCount)
     timeThead.start()
 
