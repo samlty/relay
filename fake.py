b71b376096129ac5f36f48e63c1730fb0c3f18bf
xujianhua <xujianhua@bj.xinwei.com.cn>
2017-09-05 09:57:02 +0800

    add delay 20s for fake

--- fake.py
+++ fake.py
@@ -26,6 +26,7 @@ def sendToPorts(ifname, hostname):
         for i in range(45600,45700):
             pkt = IP(src=localIp, dst=remoteIp)/UDP(sport=1194, dport=i)/data
             send(pkt)
+
     
     time.sleep(10)
 
@@ -34,6 +35,7 @@ if __name__ == "__main__":
         print("use interfaceName destip/hostname ")
         sys.exit(1)
 
+    time.sleep(20)
     t = threading.Thread(target=sendToPorts, args=(sys.argv[1],sys.argv[2],))
     t.start()
 
